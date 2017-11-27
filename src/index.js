3694a82f0b532b8d5b2a29449d5ca6e65df3665f
Canaan Seaton <cmseaton42@gmail.com>
2017-11-27 17:16:24 -0600

    work environment setup, config file added, cake preloader gifs added

--- src/index.js
+++ src/index.js
@@ -3,27 +3,32 @@ import ReactDOM from "react-dom";
 import "../style/vendor/animate/animate.min.scss";
 import "../style/style.scss";
 
-
-
 const App = () => {
+    // <img src="https://media.giphy.com/media/xUOxffodU10EVeDwcg/giphy.gif" />
+    //         <img src="https://media.giphy.com/media/xUOxeWWsAqUbGWrcPK/giphy.gif" />
+    //         <img src="https://media.giphy.com/media/xUOxfg7tsf63xReDRe/giphy.gif" />
+    //         <img src="https://media.giphy.com/media/xUOxf7hN5NIATS3wgU/giphy.gif" />
+
     return (
         <div>
-           
+            <h1>
+                ALL APPEARS TO BE <strong>GOOD</strong>
+            </h1>
         </div>
     );
 };
 
 // Preload and then mount App
-// window.addEventListener("load", () => {
-//     let preloader = document.getElementById("pre-loader");
-//     let appRoot = document.getElementById("root");
+window.addEventListener("load", () => {
+    let preloader = document.getElementById("pre-loader");
+    let appRoot = document.getElementById("root");
 
-//     let preload = setTimeout(() => {
-//         preloader.classList.add("animated", "fadeOut");
+    let preload = setTimeout(() => {
+        preloader.classList.add("animated", "fadeOut");
 
-//         let imageLoad = setTimeout(() => {
-//             preloader.style.display = "none";
-//             ReactDOM.render(<App />, appRoot);
-//         }, 650);
-//     }, 1500);
-// });
+        let imageLoad = setTimeout(() => {
+            preloader.style.display = "none";
+            ReactDOM.render(<App />, appRoot);
+        }, 650);
+    }, 1500);
+});
