f599fd0dceb8d8f360763cee9adf36b549e0f467
Canaan Seaton <cmseaton42@gmail.com>
2017-11-28 15:59:15 -0600

    added icon

--- src/components/signature.js
+++ src/components/signature.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import Waypoint from 'react-waypoint';
+import React, { Component } from "react";
+import Waypoint from "react-waypoint";
 
-import '../../style/components/signature.scss';
+import "../../style/components/signature.scss";
 
 export default class Signature extends Component {
     constructor() {
         super();
 
         this.state = {
-            loaded: false,
-            text: 'Creative Confections'
-        }
+            loaded: false
+        };
     }
 
     render() {
         return (
             <div className="cmpnt-signature">
                 <svg id="signature-svg">
-                    <text>{this.state.text}</text>
+                    <text className="signature-C1">C</text>
+                    <text className="signature-reative">reative </text>
+                    <text className="signature-C2">C</text>
+                    <text className="signature-onfections">onfections</text>
                 </svg>
             </div>
-        )
+        );
     }
 }
