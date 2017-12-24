598658c0fadb04b524d5ca2274ee197cf15ea959
Canaan Seaton <cmseaton42@gmail.com>
2017-12-24 17:40:43 -0600

    updated fb link

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -74,35 +74,27 @@ export default class Footer extends Component {
     render() {
         const { email, message } = this.state;
 
-        return (
-            <div className="cmpnt-footer container-fluid">
+        return <div className="cmpnt-footer container-fluid">
                 <div className="row justify-content-center">
                     <div className="col-sm-12 col-md-4 col-lg-3">
                         <h5 className="font-primary color-white font-weight-bold">
                             STAY CONNECTED
                         </h5>
                         <p className="footer-detail">
-                            I am proud to invite you to join my mailing list so that we can keep in
-                            touch.
+                            I am proud to invite you to join my mailing list so that we can
+                            keep in touch.
                         </p>
                         <form onSubmit={this._onSubmit} className="form-footer-contact">
-                            {message === "" ? null : <p className="footer-message">{message}</p>}
+                            {message === "" ? null : <p className="footer-message">
+                                    {message}
+                                </p>}
                             <div className="form-control">
-                                <input
-                                    className="form-control"
-                                    id="name"
-                                    type="text"
-                                    placeholder="Email Address"
-                                    onChange={this._emailChange}
-                                    value={email}
-                                />
-                                {this.state.sending == true ? (
-                                    <i className="fa fa-spinner footer-spinner" />
-                                ) : null}
+                                <input className="form-control" id="name" type="text" placeholder="Email Address" onChange={this._emailChange} value={email} />
+                                {this.state.sending == true ? <i className="fa fa-spinner footer-spinner" /> : null}
                             </div>
                         </form>
                         <div>
-                            <Icon link="#" icon="facebook" />
+                            <Icon link="https://www.facebook.com/Creative-Confections-203839296308118/" icon="facebook" />
                             <Icon link="#" icon="instagram" />
                             <Icon link="#" icon="linkedin" />
                             <Icon link="#" icon="twitter" />
@@ -113,15 +105,18 @@ export default class Footer extends Component {
                             MISSION STATEMENT
                         </h5>
                         <p className="footer-detail">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tortor
-                            metus, dignissim consectetur volutpat a, volutpat id risus. Maecenas
-                            bibendum suscipit leo rhoncus porttitor. Fusce accumsan porta felis in
-                            elementum. Nulla gravida dolor convallis mauris semper bibendum.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                            tortor metus, dignissim consectetur volutpat a, volutpat id
+                            risus. Maecenas bibendum suscipit leo rhoncus porttitor. Fusce
+                            accumsan porta felis in elementum. Nulla gravida dolor convallis
+                            mauris semper bibendum.
                         </p>
                     </div>
                     <div className="col-sm-12 col-md-2 col-lg-1 text-center">
                         <div className="directory-container">
-                            <h5 className="font-primary color-white font-weight-bold">DIRECTORY</h5>
+                            <h5 className="font-primary color-white font-weight-bold">
+                                DIRECTORY
+                            </h5>
                             <ul className="nav flex-column directory text-left">
                                 <li className="nav-item">
                                     <Link to="/">Home</Link>
@@ -136,8 +131,7 @@ export default class Footer extends Component {
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            </div>;
     }
 }
 
